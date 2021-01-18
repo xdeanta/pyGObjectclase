477cb5787114dc2261d60b27010afa21087d03ec
dam2a <xdeantabelisario@danielcastelao.org>
2021-01-18 11:27:10 +0100

    Ejemplo 14, listo

--- example14.py
+++ example14.py
@@ -3,16 +3,33 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 columnas = ["Nombre", "Apellido", "Numero de telefono"]
-agendaTel= ["Manuel" , "Gil", "986 231 543"]
+agendaTel = [["Manuel", "Gil", "986 345 678"],
+             ["Ana", "Sáez", "607 891 891"],
+             ["Rosa", "Cendón", "678 432 456"],
+             ["Raúl", "Pérez", "651 327 453"],
+             ["Óscar", "Vila", "986123 321"]]
+
 
 class EjemploGtkTreeViewTelefonos(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Saludo usando POO")
+        Gtk.Window.__init__(self, title="Agenda Telefonica")
         self.set_size_request(200,100)
 
         mod_agenda = Gtk.ListStore(str,str,str)
         for persona in agendaTel:
-            mod_agenda.append([persona])
+            mod_agenda.append(persona)
+
+        tablaAgenda = Gtk.TreeView(model=mod_agenda)
+
+        i = 0
+        for columna in columnas:
+            celdaRenderer = Gtk.CellRendererText()
+            col = Gtk.TreeViewColumn(columna, celdaRenderer, text=i)
+            tablaAgenda.append_column(col)
+            i = i + 1
+
+        self.add(tablaAgenda)
+
         self.connect("delete-event", Gtk.main_quit)
         self.show_all()
 
